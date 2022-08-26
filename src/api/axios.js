60867957651ddfe31209cd9cb1350d25bf4f7410
abh <themechanic002@gmail.com>
2022-08-27 00:15:32 +0900

    how to create multiple instances...?

--- src/api/axios.js
+++ src/api/axios.js
@@ -5,7 +5,13 @@ import CryptoJS from 'crypto-js'
 // var p_apikey = "e2fd3ebb772c7f743f4bdd86a63d91ba";
 // var p_hash = CryptoJS.MD5(p_ts + "62358b2dbd6adc8aed0e6d49a13575181a332ee9" + p_apikey);
 
-
+const setAuthToken = (token) => {
+  if (token) {
+    axios.defaults.headers.common.Authorization = `Bearer $`;
+  } else {
+    delete axios.defaults.headers.common.Authorization;
+  }
+};
 
 const instance1 = axios.create({
   baseURL: "http://gateway.marvel.com",
@@ -62,4 +68,5 @@ const instance5 = axios.create({
   }
 });
 
-export default {instance1, instance2, instance3, instance4, instance5};
+export default setAuthToken;
+export {instance1, instance2, instance3, instance4, instance5};
